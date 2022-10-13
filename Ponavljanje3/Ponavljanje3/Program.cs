13b3fc685daf9898dc15c6057a6573c8721091f2
MatejPemper <90246393+MatejPemper@users.noreply.github.com>
2022-10-13 15:10:09 +0200

    Add files via upload

---
+++ Ponavljanje3/Ponavljanje3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponavljanje3
+{
+    internal class Program
+    {
+
+        class KlasaX
+        {
+
+            int[] array = new int[100];
+
+            int index;
+
+            public int Index { get => index; set => index = value; }
+
+            public int Dodjela(int[] array, int i)
+            {
+                i = Index;
+                return array[Index];
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            int[] array = new int[100];
+            KlasaX x = new KlasaX();
+            for (int i = 0; i < 100; i++)
+            {
+                x.Index = i;
+                x.Dodjela(array, i);
+            }
+            Console.WriteLine(array[50]);
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
